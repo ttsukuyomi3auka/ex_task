bbf109012acab561d3f0369394eada6d11352b3a
tsukuyomi <tsumi@mail>
2023-10-04 19:07:40 +0500

    init

---
+++ bin/ex_task.dart
@@ -0,0 +1,6 @@
+import 'package:ex_task/ex_task.dart' as ex_task;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ex_task.calculate()}!');
+  
+}
---
+++ test/ex_task_test.dart
@@ -0,0 +1,8 @@
+import 'package:ex_task/ex_task.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
